651aa30b098fcc2c2b8486658915e72a6f6d28cb
ValerDev <valer.val7@gmail.com>
2021-04-27 17:49:12 +0400

    changes

---
+++ src/components/content/Menu_2.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import "../../index.scss"
+import "../../assets/content/menu_2.scss"
+class Menu_2 extends React.Component {
+    render() {
+        return (
+            <section id='menu_2'>
+                <h1>Menu_2</h1>
+            </section>
+        )
+    }
+}
+
+export default Menu_2;
